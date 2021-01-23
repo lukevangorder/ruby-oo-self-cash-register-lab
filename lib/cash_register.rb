29821f0b7f442f7230746b32d1e086dbbaf9823e
lukevangorder <lukevangorder@gmail.com>
2021-01-23 14:28:49 -0500

    commit tree 7ee6631aec03ad5e659303facccb4446822a5bb7

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
-
+class CashRegister
+    attr_accessor :total, :discount
+    def initialize(employee_discount=0)
+        @total=0
+        @discount=employee_discount
+        @cart = []
+    end
+    def add_item(title, price, quantity=1)
+        @last_ring = price*quantity
+        @total = @last_ring + @total
+        quantity.times {|i| @cart.push(title)}
+    end
+    def apply_discount
+        if @discount>0  
+            savings = @total*@discount*0.01
+            @total -= savings.round
+            return "After the discount, the total comes to $#{@total}."
+        else
+            return "There is no discount to apply."
+        end
+    end
+    def items
+        @cart
+    end
+    def void_last_transaction
+        @cart.pop
+        @total -= @last_ring
+    end
+end
